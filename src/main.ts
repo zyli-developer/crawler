06ea0e1aae0ce6ba7ccb90f0087b4a2b5887b440
zyli <lizhenyu@0613@126.com>
2023-03-22 14:57:51 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
-// You don't need to import RequestQueue anymore
-import { CheerioCrawler } from 'crawlee';
+import { PlaywrightCrawler } from 'crawlee';
 
-const crawler = new CheerioCrawler({
-  maxRequestsPerCrawl: 20,
-    async requestHandler({ $, request , enqueueLinks }) {
-        const title = $('title').text();
-        console.log(`The title of "${request.url}" is: ${title}.`);
-        await enqueueLinks();
-    }
-})
+const crawler = new PlaywrightCrawler({
+    async requestHandler({ page }) {
+        // Here we don't wait for the selector and immediately
+        // extract the text content from the page.
+        const actorText = await page.$eval('.ActorStoreItem', (el) => {
+            return el.textContent;
+        });
+        console.log(`ACTOR: ${actorText}`);
+    },
+});
 
-// Start the crawler with the provided URLs
-await crawler.run(['https://crawlee.dev']);
-await crawler.run(['https://www.baidu.com']);
+await crawler.run(['https://apify.com/store']);
